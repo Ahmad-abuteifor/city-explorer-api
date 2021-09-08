475d8caaf084c89b40cb91f5ea862f79eaf7cf89
Ahmad-abuteifor <ana1ahmad10@gmail.com>
2021-09-08 15:39:43 +0300

    lab 09 and update for lab 08,07

--- server.js
+++ server.js
@@ -6,56 +6,11 @@ app.use(cors())
  
 require('dotenv').config();
 const axios = require('axios'); // require the package
-
-
+const getwrather = require('./controller/wether.controller');
+const getmovie = require('./controller/movie.control');
 const PORT=process.env.PORT
-const weather=require('./data/weather.json')
-const WEATHER_API_KEY=process.env.WEATHER_API_KEY
 
 
-class Forecast {
-  constructor(date,description){
-  this.date=date
-  this.description=description
-
-
-}}
-
-
-
-
-
-app.get('/weathers', async(request,res)=>{ 
-  const  city = request.query.city_name;
-
-
-
-  const lon=request.query.lon
-  const lat=request.query.lat
-
-  const  weatherUrl=`https://api.weatherbit.io/v2.0/forecast/daily?`
-  const weatherUrlUsed=await axios.get(`${weatherUrl}lon=${lon}&lat=${lat}&key=${WEATHER_API_KEY}`)
-
-  res.json(weatherUrlUsed)
-  
-// const checkArray=weather.find(item=>{
-// return item.city_name.toLowerCase()===city.toLowerCase()
-
-// })
-
-// if (checkArray){
-// let newArray=checkArray.data.map(item=>{
-//   return new Forecast(item.datetime,item.weather.description)
-// })
-// res.json(newArray)
-// }else {
-//  res.json('no data ')
-// }
-
-
-}
-
-)
-
-
+app.get('/weathers',getwrather)
+app.get('/movies', getmovie)
 app.listen(PORT)
